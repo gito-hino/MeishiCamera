09e961109624dede5a891c05e60d6c7c709c5d77
Akito Hino <mynameisakito55@gmail.com>
2026-02-19 00:35:31 +0900

    chore: Flutter 3.41.0 + Riverpod初期プロジェクト作成

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:app/main.dart';
-
-void main() {
-  testWidgets('Hello world text smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
-    expect(find.text('hello world'), findsOneWidget);
-  });
-}
